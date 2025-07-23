5f6b11b3b63be3660aed9c7a69687a356cc20a81
Grzegorz R Juszczak <gjuszczak@yahoo.com>
2025-07-23 16:55:44 +0200

    Update R_test of additional IDs in Ensembl Human.R

--- R_test of additional IDs in Ensembl Human.R
+++ R_test of additional IDs in Ensembl Human.R
@@ -1,3 +1,9 @@
+########################################################################################################
+#### Trial dataset for this R script can be downloaded from                                         ####
+#### https://data.mendeley.com/datasets/454s2vw255/1/files/79a080ef-3dfb-45e4-88d0-c9f1274d9b9c     ####
+#### The data should be extracted from the compressed folder before running the script.             #### 
+########################################################################################################
+
 mart_export_1 <- read.delim('mart_export.txt', sep=',', header = TRUE) # data importing
 head(mart_export_1, n=20) # showing first n rows
 
