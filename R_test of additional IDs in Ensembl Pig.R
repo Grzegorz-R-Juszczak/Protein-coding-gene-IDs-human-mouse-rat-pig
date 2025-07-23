e2da6dddc3d40dd189b7b7529a1f16245c34f727
Grzegorz R Juszczak <gjuszczak@yahoo.com>
2025-07-23 17:00:10 +0200

    Update R_test of additional IDs in Ensembl Pig.R

--- R_test of additional IDs in Ensembl Pig.R
+++ R_test of additional IDs in Ensembl Pig.R
@@ -1,3 +1,9 @@
+########################################################################################################
+#### Trial dataset for this R script can be downloaded from                                         ####
+#### https://data.mendeley.com/datasets/454s2vw255/1/files/e079cac4-6f67-45cd-b316-43004917f70c     ####
+#### The data should be extracted from the compressed folder before running the script.             #### 
+########################################################################################################
+
 mart_export_1 <- read.delim('mart_export.txt', sep=',', header = TRUE) # data importing
 head(mart_export_1, n=20) # showing first n rows
 
